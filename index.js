2f369bad3854251079c4e27577c8a01ef0638b11
Bhagyashri <bhagyashrigunaga@gmail.com>
2026-04-12 15:38:44 +0530

    job tracker project

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const jobRoutes = require("./routes/jobRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/jobtracker")
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+app.use("/jobs", jobRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
